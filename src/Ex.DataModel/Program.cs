de66373ba86562fa41bbad2fc954846b210cc314
Leandro Camargo <leandrocamaargo@hotmail.com>
2019-11-27 22:52:18 -0400

    Add project files.

---
+++ src/Ex.DataModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex.DataModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
